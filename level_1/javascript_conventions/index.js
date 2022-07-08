ffadc51e1d10cee73ac78765d00fe847e3ba983d
Erik Tycksen <eriktycksen@gmail.com>
2022-07-08 12:08:21 -0600

    adjusted file names for easier access and navigation

---
+++ level_1/javascript_conventions/index.js
@@ -0,0 +1,64 @@
+// JS Conventions
+
+// casing - camelCased
+
+var lastName or lName 
+var my LocationIsinUtah = true
+
+// white space
+
+for(var i = 0; i < 10; i++)
+
+NOT 
+
+for(var i=0;i<10;i++)
+
+// naming
+
+var firstName = erik
+var lastName = tycksen
+
+NOT
+
+var fN = erik
+var lN = tycksen
+
+var friends = []
+var isDone = true
+var isGameOver = false
+
+function sum(num1, num2){
+
+}
+
+// braces
+
+var person = {
+    address: {
+
+    }
+}
+
+// tab (indenting)
+
+if(2 === 2){
+    console.log("they are equal")
+    if(3 === 3){
+        console.log("They are equal")
+    }
+}
+
+// HTML tags
+
+    // <div> not <DIV> or <my div>
+
+// comments
+
+    /*
+        anything in betwene these are commented out
+    */
+
+// unix casing
+    // silly_cia - yes
+    // Silly CIA - no
+    //silly cia - no
